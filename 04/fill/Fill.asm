72b628cccfab34106f6889997a8191fff9c1be2f
jorgeriveratorres <jerivera@smu.edu>
2021-03-29 18:37:19 -0500

    Update Fill.asm
    KBD CHECK: Meaning it looks into what keys are pressed in order to choose between white and black

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,17 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(RESTART)
+	@SCREEN
+	D = A
+	@0
+	M = D
+
+(KBDCHECK)
+	@KBD
+	D = M
+	@BlACK
+	D;JGT
+	@WHITEE
+	D;JEQ
