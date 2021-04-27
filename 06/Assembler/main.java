630821b386c1cd07575d991a0dce6f34c5c01606
jorgeriveratorres <jerivera@smu.edu>
2021-04-27 13:14:14 -0500

    Update main.java

--- 06/Assembler/main.java
+++ 06/Assembler/main.java
@@ -1,13 +1,40 @@
 public class Code {
 	
 	public static String Dest(String dest) {
-		String result = "";
+		String results = "";
 		switch(dest) {
 
 			case "M":
 				result = "001";
 				break;
 
+			case "D":
+				result = "010";
+				break;
+
+			case "MD":
+				result = "011";
+				break;
+
+			case "A":
+				result = "100";
+				break;
+
+			case "AM":
+				result = "101";
+				break;
+
+			case "AD":
+				result = "110";
+				break;
+
+			case "AMD":
+				result = "111";
+				break;
+
+			default:
+				result = "000"
 		}
+		return results;
 	}
 }
