4992636d0d46278d7c07389b56a83f1e7de98e56
jorgeriveratorres <jerivera@smu.edu>
2021-04-28 21:13:37 -0500

    Update Asemblaje.java
    Command Switch

--- 06/Assembler/Asemblaje.java
+++ 06/Assembler/Asemblaje.java
@@ -87,4 +87,70 @@ public class Asemblaje {
 		}
 	}
 
-	
+	public char commandToBinary(String line) throws Exception {
+		String command = line.replaceAll(".*=", "");
+		command = command.replaceAll(";.*", "");
+
+		switch(command) {
+			case "0": 
+				return 0b0101010;
+			case "1":
+				return 0b0111111;
+			case "-1":
+				return 0b0111010;
+			case "D":
+				return 0b0001100;
+			case "A":
+				return 0b0110000;
+			case "!D":
+				return 0b0001101;
+			case "!A":
+				return 0b0110001;
+			case "-D":
+				return 0b0001111; 
+			case "-A":
+				return 0b0110011;
+
+			case "D+1":
+				return 0b0011111;
+			case "A+1":
+				return 0b0110111;
+			case "D-1":
+				return 0b0001110;
+			case "A-1":
+				return 0b0110010;
+			case "D+A":
+				return 0b0000010;
+			case "D-A":
+				return 0b0010011;
+			case "A-D":
+				return 0b0000111;
+			case "D&A":
+				return 0b0000000;
+			case "D|A":
+				return 0b0010101;
+
+			case "M": 
+				return 0b1110000;
+			case "!M":
+				return 0b1110001;
+			case "-M":
+				return 0b1110011;
+			case "M+1":
+				return 0b1110111;
+			case "M-1":
+				return 0b1110010;
+			case "D+M":
+				return 0b1000010;
+			case "D-M":
+				return 0b1010011;
+			case "M-D":
+				return 0b1000111;
+			case "D&M":
+				return 0b1000000;
+			case "D|M":
+				return 0b1010101;
+			default:
+				throw new Exception("Command could not be converted");
+		}
+	}
